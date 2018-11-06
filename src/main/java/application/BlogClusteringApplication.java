284746b23dbb426875a92a62e4d68e48422e34a8
Lucas Wik <lucas.wik@ist.com>
2018-11-06 18:27:22 +0100

    Initial projet setup.

---
+++ src/main/java/application/BlogClusteringApplication.java
@@ -0,0 +1,20 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BlogClusteringApplication {
+
+	public static void main(String[] args){
+		SpringApplication.run(BlogClusteringApplication.class, args);
+	}
+
+	@GetMapping("/")
+	String getHello(){
+		return "hello";
+	}
+}
---
+++ src/main/java/application/dataset/handler/DatasetHandler.java
@@ -0,0 +1,5 @@
+package application.dataset.handler;
+
+
+public class DatasetHandler {
+}
