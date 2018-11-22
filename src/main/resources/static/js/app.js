01c6dea2c3a13e26ba5311c60b89ec3371ff7f45
Lucas Wik <lucas.wik@ist.com>
2018-11-22 19:36:51 +0100

    Removed unused JS

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,10 +3,8 @@
 let result = document.getElementById("result");
 
 document.getElementById("submit").addEventListener("click", function(){
-    console.log("Event triggered");
 
     let amountOfClusters = document.getElementById("amountOfClusters").value;
-    console.log(amountOfClusters);
 
     if(isNaN(amountOfClusters)){
         result.innerHTML="You must provide a number";
@@ -21,15 +19,12 @@ document.getElementById("submit").addEventListener("click", function(){
 
     http.onreadystatechange=function(){
         if(this.readyState===4 && this.status===200){
-            //console.log(http.responseText);
             let resultTable = "<table> <tr><th>Centroid</th><th>Assigned blogs</th> </tr>"
 
             let requestResult = JSON.parse(http.responseText);
-            console.log(requestResult);
+            
             for(let i = 0; i < amountOfClusters; i++){
                 resultTable += "<tr> <td>" + Object.entries(requestResult)[i][1].title +"</td> <td>";
-
-                //console.log(Object.entries(requestResult)[i][1]);
 
                 let blogs = Object.entries(requestResult)[i][1].blogs;
                 for(let k = 0; k < blogs.length; k++){
@@ -43,5 +38,4 @@ document.getElementById("submit").addEventListener("click", function(){
             result.innerHTML = resultTable;
         }
     }
-    //document.getElementById("demo").innerHTML = "Hello World";
 });
